541d82adf6ef23379c86eb6a0a1395c64f25a460
Kushal Shah <dev@kushalshah.com.np>
2024-11-17 18:39:00 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import ImgGen from './components/ImgGen'
+function App() {
+  
+  return (
+    <>
+    <ImgGen/>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+// AIzaSyBZFjABx0KUosTtO-LlXWhF9ZzET5h3_zE
---
+++ src/components/ImgGen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ImgGen.css'
+const ImgGen = () => {
+  return (
+    <div className='heading'>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default ImgGen
